f78bd3834422d8cf0d9e0a317ea101d50f669bda
david-barrett <david.barrett.0@gmail.com>
2026-05-11 14:18:20 +0100

    favicon

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,7 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import favicon from "./assets/mop_logo.jpeg";
 import "./index.css";
 import App from "./App.tsx";
+
+const icon = document.createElement("link");
+icon.rel = "icon";
+icon.type = "image/jpeg";
+icon.href = favicon;
+document.head.appendChild(icon);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
